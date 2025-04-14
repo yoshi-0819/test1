d18dd0e40d91711f8591fded7eb9423a12b9c875
yoshi-0819 <majime0819@gmail.com>
2025-04-14 18:21:18 +0900

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace 国の情報を取得できる_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
